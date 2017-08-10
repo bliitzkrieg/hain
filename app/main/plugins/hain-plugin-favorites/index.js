e0e84d634ccca6900416ea15fad977afb54606ba
Luca DeCaprio <lucamatthewdecaprio@gmail.com>
2017-08-09 23:18:30 -0400

    Added plugin to quick add some favorite sites

---
+++ app/main/plugins/hain-plugin-favorites/index.js
@@ -0,0 +1,69 @@
+'use strict';
+
+const lo_first = require('lodash.first');
+
+module.exports = (context) => {
+  const shell = context.shell;
+  const app = context.app;
+  const indexer = context.indexer;
+
+  const favorites = [
+      {
+          key: 'MMO',
+          id: 'http://www.mmo-champion.com/content/',
+          primaryText: 'http://www.mmo-champion.com/content/',
+          secondaryText: 'World Of Warcraft News',
+      },
+      {
+          key: 'EMAIL',
+          id: 'https://mail.google.com/mail/u/0/',
+          primaryText: 'https://mail.google.com/mail/u/0/',
+          secondaryText: 'Personal Email',
+      },
+      {
+          key: 'WORK',
+          id: 'https://mail.google.com/mail/u/2/',
+          primaryText: 'https://mail.google.com/mail/u/2/',
+          secondaryText: 'Work Email',
+      },
+      {
+          key: 'YT',
+          id: 'https://www.youtube.com/feed/subscriptions',
+          primaryText: 'https://www.youtube.com/feed/subscriptions',
+          secondaryText: 'YouTube Videos',
+      },
+      {
+          key: 'GITHUB',
+          id: 'https://github.com/',
+          primaryText: 'https://github.com/',
+          secondaryText: 'Github',
+      },
+      {
+          key: 'HACKER',
+          id: 'https://news.ycombinator.com/',
+          primaryText: 'https://news.ycombinator.com/',
+          secondaryText: 'Hacker News',
+      }
+  ];
+
+  function startup() {
+    indexer.set('favorite-fetcher', parseFavoriteForIndexer);
+  }
+
+  function parseFavoriteForIndexer(query) {
+    const search = query.toUpperCase();
+    return favorites.filter(favorite => favorite.key === search);
+  }
+
+  function execute(id, payload, extra) {
+    const protocol_re = /https?:\/\//i;
+    let url = id;
+    if (protocol_re.test(url) === false) {
+      url = `http://${url}`;
+    }
+    shell.openExternal(url);
+    app.close();
+  }
+
+  return { startup, execute };
+};
